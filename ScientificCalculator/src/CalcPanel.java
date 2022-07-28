e606c5870816243c43ec03106cef59c2a992592d
xicoc <xicoc@LAPTOP-HINT7M6I>
2022-07-28 15:26:23 +0100

    Change Button to JButton and setFont on JTextField

--- ScientificCalculator/src/CalcPanel.java
+++ ScientificCalculator/src/CalcPanel.java
@@ -1,10 +1,10 @@
 import java.awt.BorderLayout;
-import java.awt.Button;
+import java.awt.Font;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-//import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class CalcPanel {
@@ -12,7 +12,7 @@ public class CalcPanel {
 	private JFrame cFrame;
 	private JTextField cText;
 	private Evaluator eval;
-	Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bplus, bac,
+	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bplus, bac,
 				bminus, bdiv, bmult, bequal, bcos, bsin, btan,
 				bdec, bdel, be, bpi, brightp, bleftp;
 	
@@ -21,7 +21,7 @@ public class CalcPanel {
 		cPanel = new JPanel(new BorderLayout());
 		cText = new JTextField();
 		eval = new Evaluator();
-		
+		cText.setFont(new Font ("TimesRoman",0,25));
 		cText.setEditable(false);
 		cText.setHorizontalAlignment(JTextField.RIGHT);
 		cPanel.setLayout(new GridLayout(0,5, 2, 2));
@@ -29,36 +29,36 @@ public class CalcPanel {
 		
 		cFrame.add(cText, BorderLayout.PAGE_START);
 		cFrame.add(cPanel, BorderLayout.CENTER);
-		cFrame.setSize(300, 400);
+		cFrame.setSize(310, 450);
 		cFrame.setVisible(true);
 	}
 	
 	private void createButtons() {
-		b1 = new Button("1");
-		b2 = new Button("2");
-		b3 = new Button("3");
-		b4 = new Button("4");
-		b5 = new Button("5");
-		b6 = new Button("6");
-		b7 = new Button("7");
-		b8 = new Button("8");
-		b9 = new Button("9");
-		b0 = new Button("0");
-		bplus = new Button("+");
-		bminus = new Button("-");
-		bdiv = new Button("÷");
-		bmult = new Button("×");
-		bequal = new Button("=");
-		bcos = new Button("cos");
-		bsin = new Button("sin");
-		btan = new Button("tan");
-		bdel = new Button("del");
-		bac = new Button("AC");
-		bdec = new Button(".");
-		bleftp = new Button("(");
-		brightp = new Button(")");
-		bpi = new Button("π");
-		be = new Button ("e");
+		b1 = new JButton("1");
+		b2 = new JButton("2");
+		b3 = new JButton("3");
+		b4 = new JButton("4");
+		b5 = new JButton("5");
+		b6 = new JButton("6");
+		b7 = new JButton("7");
+		b8 = new JButton("8");
+		b9 = new JButton("9");
+		b0 = new JButton("0");
+		bplus = new JButton("+");
+		bminus = new JButton("-");
+		bdiv = new JButton("÷");
+		bmult = new JButton("×");
+		bequal = new JButton("=");
+		bcos = new JButton("cos");
+		bsin = new JButton("sin");
+		btan = new JButton("tan");
+		bdel = new JButton("del");
+		bac = new JButton("AC");
+		bdec = new JButton(".");
+		bleftp = new JButton("(");
+		brightp = new JButton(")");
+		bpi = new JButton("π");
+		be = new JButton ("e");
 		
 		cPanel.add(bsin); cPanel.add(bcos); cPanel.add(btan); cPanel.add(bpi); cPanel.add(be); 
 		cPanel.add(b1); cPanel.add(b2); cPanel.add(b3); cPanel.add(bdel);  cPanel.add(bac);
