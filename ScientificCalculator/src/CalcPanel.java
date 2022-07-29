c5cc34768339ee3cea3660479cc68e15edeb4675
xicoc <xicoc@LAPTOP-HINT7M6I>
2022-07-29 13:55:38 +0100

    Working calculator, atleats simple operations and functions

--- ScientificCalculator/src/CalcPanel.java
+++ ScientificCalculator/src/CalcPanel.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -12,6 +14,7 @@ public class CalcPanel {
 	private JFrame cFrame;
 	private JTextField cText;
 	private Evaluator eval;
+	private boolean result;
 	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bplus, bac,
 				bminus, bdiv, bmult, bequal, bcos, bsin, btan,
 				bdec, bdel, be, bpi, brightp, bleftp;
@@ -21,15 +24,19 @@ public class CalcPanel {
 		cPanel = new JPanel(new BorderLayout());
 		cText = new JTextField();
 		eval = new Evaluator();
+		result = false;
 		cText.setFont(new Font ("TimesRoman",0,25));
 		cText.setEditable(false);
 		cText.setHorizontalAlignment(JTextField.RIGHT);
-		cPanel.setLayout(new GridLayout(0,5, 2, 2));
-		createButtons();
+		cPanel.setLayout(new GridLayout(0,5, 5, 5));
 		
+		createButtons();
+		cPanel.setBackground(Color.darkGray);
+		cPanel.setPreferredSize(new Dimension(310, 200));
 		cFrame.add(cText, BorderLayout.PAGE_START);
 		cFrame.add(cPanel, BorderLayout.CENTER);
-		cFrame.setSize(310, 450);
+		cFrame.setSize(400, 300);
+		cFrame.setResizable(false);
 		cFrame.setVisible(true);
 	}
 	
@@ -68,75 +75,99 @@ public class CalcPanel {
 	
 	
 		b1.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "1");
 		});
 		b2.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "2");
 		});
 		b3.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "3");
 		});
 		b4.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "4");
 		});
 		b5.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "5");
 		});
 		b6.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "6");
 		});
 		b7.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "7");
 		});
 		b8.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "8");
 		});
 		b9.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "9");
 		});
 		b0.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "0");
 		});
 		bplus.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "+");
 		});
 		bminus.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "-");
 		});
 		bmult.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "×");
 		});
 		bdiv.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "÷");
 		});
 		bdec.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + ".");
 		});
 		bleftp.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "(");
 		});
 		brightp.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + ")");
 		});
 		bsin.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "sin");
 		});
 		bcos.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "cos");
 		});
 		btan.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "tan");
 		});
 		bdel.addActionListener(e -> {
-			cText.setText(cText.getText().substring(0, cText.getText().length() - 1));
+			if(!cText.getText().equals(""))
+				cText.setText(cText.getText().substring(0, cText.getText().length() - 1));
 		});
 		bac.addActionListener(e -> {
+			afterEqual();
 			cText.setText("");
 		});
 		bpi.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "π");
 		});
 		be.addActionListener(e -> {
+			afterEqual();
 			cText.setText(cText.getText() + "e");
 		});
 		bequal.addActionListener(e -> {
@@ -144,10 +175,17 @@ public class CalcPanel {
 			String replacedExp = expression.replace("×","*").replace("÷", "/");
 			Double x = eval.evaluate(replacedExp);
 			cText.setText(x.toString());
+			result = true;
 		});
 
 	
 	}
 	
+	public void afterEqual() {
+		if(result) {
+			cText.setText("");
+			result = false;
+		}
+	}
 	
 }
--- ScientificCalculator/src/Evaluator.java
+++ ScientificCalculator/src/Evaluator.java
@@ -1,9 +1,92 @@
+import javax.management.RuntimeErrorException;
 
 public class Evaluator {
-
+	int currentPos = -1;
+	String expression;
+	char ch;
+	
 	public Double evaluate(String exp) {
-		// TODO Auto-generated method stub
-		return 0.0;
+		resetValues(exp);		
+		nextChar();
+		Double x = evalAddSub();
+		if(currentPos >= expression.length())
+			throw new RuntimeException("Syntax Error");
+		return x;
+	}
+
+	private void nextChar() {
+		if(++currentPos < expression.length())
+			throw new RuntimeException("Syntax Error");
+		ch = expression.charAt(currentPos);
+	}
+	
+	private Double evalAddSub() {
+		Double x = evalMultDiv();
+		while(true) 
+			if(removeOperator('+')) x += evalMultDiv();
+			else if (removeOperator('-')) x -= evalMultDiv();
+			else return x;
+	}
+
+	private Double evalMultDiv() {
+		Double x = evalFactors();
+		while(true) 
+			if(removeOperator('*')) x *= evalFactors();
+			else if (removeOperator('/')) x /= evalFactors();
+			else return x;
+	}
+
+	private Double evalFactors() {
+		Double x = 0.0;
+		
+		if(removeOperator('+')) x = evalFactors();
+		if(removeOperator('-')) x = -evalFactors();
+		
+		int startPos = currentPos;
+		if(removeOperator('(')) {
+			x = evalAddSub();
+			removeOperator(')');
+		}
+		else if(Character.isDigit(ch)) {
+			while(Character.isDigit(ch) || ch == '.') nextChar();
+			x = Double.parseDouble(expression.substring(startPos,currentPos));
+		}
+		else if(Character.isLetter(ch)) {
+			while(Character.isLetter(ch)) nextChar();
+			 String func = expression.substring(startPos,currentPos);
+			switch(func){
+				case "sin":
+					x = Math.sin(evalFactors());
+					break;
+				case "cos":
+					x = Math.cos(evalFactors());
+					break;
+				case "tan":
+					x = Math.tan(evalFactors());
+					break;
+				case "π":
+					x = Math.PI;
+				case "e":
+					x = Math.E;
+				default:
+					throw new RuntimeException("Unexpected: " + func);
+			}
+		}
+		return x;
+	}
+	
+	private boolean removeOperator(char op) {
+		if(ch == op) {
+			nextChar();
+			return true;
+		}
+		return false;
+	}
+	
+	private void resetValues(String str) {
+		expression = str;
+		currentPos = -1;
+		ch = ' ';
 	}
 	
 }
